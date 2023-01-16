6beeae7bd1aa39f1d64404ab461b2b52d591126e
robrechtme <robrechtmeersman@hotmail.com>
2023-01-16 17:33:20 +0100

    feat: add bitrate and fps support

--- ios/RecordScreen.h
+++ ios/RecordScreen.h
@@ -11,6 +11,8 @@
     @property (assign, nonatomic) int screenWidth;
     @property (assign, nonatomic) int screenHeight;
     @property (assign, nonatomic) BOOL enableMic;
+    @property (assign, nonatomic) int fps;
+    @property (assign, nonatomic) int bitrate;
 
     @property (nonatomic) AVAssetWriter *writer;
     @property BOOL encounteredFirstBuffer;
